d618c49c1381f5f0c119b76480c49305b2e59026
Virgile Robles <virgile.robles@protonmail.ch>
2021-06-06 06:16:44 +0200

    Add polybar launcher

---
+++ menus/launch_polybar.ml
@@ -0,0 +1,23 @@
+open Feather
+
+let bar_name = "main"
+
+let kill_old () = process "killall" [ "-q"; "polybar" ] |> run
+
+let current_env =
+  let parse s = Scanf.sscanf s "%s@=%s" (fun a b -> (a, b)) in
+  Unix.environment () |> Array.to_list
+  |> List.map parse
+
+let launch_new () =
+  let launch_one mon =
+    Printf.printf "Launching a bar on %s\n" mon;
+    let env = ("MONITOR", mon) :: current_env in
+    process "polybar" [ "--reload"; bar_name ] |> run_bg ~env
+  in
+  let monitors = process "polybar" [ "-m" ] |. cut ~d:':' 1 |> collect_lines in
+  List.iter launch_one monitors
+
+let _ =
+  kill_old ();
+  launch_new ()
